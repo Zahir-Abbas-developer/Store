be44a59f44053436c4e3fba770c7661a070997fc
zahir <zahirr060@gmail.com>
2023-05-24 17:23:13 +0500

    #updated code

--- src/layout/Sidebar/SidebarData.tsx
+++ src/layout/Sidebar/SidebarData.tsx
@@ -38,466 +38,70 @@ export const getSidebarMenues = (role: any, permissions: any) => {
 
   const SidebarMenues = [
 
-    // ######################### Admin Sidebar Items  #########################
-    {
-      label: "Dashboard",
-      link: "/dashboard",
-      icon: <Dashboard />,
-      role: [ROLES.admin],
-    },
-    {
-      label: "Client Manager",
-      link: "/client-manager",
-      icon: <Client />,
-      role: [ROLES.admin],
-    },
-    {
-      label: "Onboarding",
-      link: "/onboarding/carer",
-      icon: <OnBoarding />,
-      role: [ROLES.admin],
-      children: [
-        {
-          label: "Carer",
-          link: "/onboarding/carer",
-          icon: "",
-        },
-        {
-          label: "Care Cordinator",
-          link: "/onboarding/care-coordinator",
-          icon: "",
-        },
-        {
-          label: "Training Instructor",
-          link: "/onboarding/training-instructor",
-          icon: "",
-        },
-      ],
-    },
-    {
-      label: "Staff Manager",
-      link: "/staff-manager",
-      icon: <Manager />,
-      role: [ROLES.admin],
-    },
-    {
-      label: "Staff Allocation",
-      link: "/staff-allocation",
-      icon: <Allocation />,
-      role: [ROLES.admin],
-    },
-    {
-      label: "Staff Booking",
-      link: "/staff-booking",
-      icon: <Booking />,
-      role: [ROLES.admin],
-    },
-    {
-      label: "Booking Calendar",
-      link: "/booking-calendar",
-      icon: <Calendar />,
-      role: [ROLES.admin],
-    },
-    {
-      label: "Shift Manager",
-      link: "/shift-manager",
-      icon: <Time />,
-      role: [ROLES.admin],
-    },
-    {
-      label: "Unpublished Shift",
-      link: "/unpublished-shift",
-      icon: <Users />,
-      role: [ROLES.admin],
-    },
-    {
-      label: "Finance",
-      link: "/finance",
-      icon: <Finance />,
-      role: [ROLES.admin],
-      children: [
-        {
-          label: "Setup",
-          link: "/finance/setup",
-          icon: "",
-        },
-        {
-          label: "Invoice",
-          link: "/finance/invoice",
-          icon: "",
-        },
-        {
-          label: "Staff Payment Details",
-          link: "/finance/staff-payment-details",
-          icon: "",
-        },
-        {
-          label: "Client Payment Details",
-          link: "/finance/client-payment-details",
-          icon: "",
-        },
-        {
-          label: "Reports",
-          link: "/finance/reports",
-          icon: "",
-        },
-      ],
-    },
 
-    // ######################### Carer Sidebar Items #########################
+    // #######################  Sidebar Items #######################  
     {
-      label: "Dashboard",
-      link: "/carer-dashboard",
-      icon: <Dashboard />,
-      role: [ROLES.carer],
-    },
-    {
-      label: "Profile",
-      link: "/carer-profile",
-      icon: <Profile />,
-      role: [ROLES.carer],
-    },
-    {
-      label: "Shift Details",
-      link: "/shift-details/available-shifts",
-      icon: <ShiftDetails />,
-      role: [ROLES.carer],
-      children: [
-        {
-          label: "Available Shifts",
-          link: "/shift-details/available-shifts",
-          icon: "",
-        },
-        {
-          label: "Upcoming Shifts",
-          link: "/shift-details/upcoming-shifts",
-          icon: "",
-        },
-        {
-          label: "Completed Shifts",
-          link: "/shift-details/completed-shifts",
-          icon: "",
-        },
-        {
-          label: "Cancelled Shifts",
-          link: "/shift-details/cancelled-shifts",
-          icon: "",
-        },
-      ],
-    },
-    {
-      label: "My Calendar",
-      link: "/carer-my-calendar",
-      icon: <Calendar />,
-      role: [ROLES.carer],
-    },
-    // {
-    //   label: "Booking Calendar",
-    //   link: "/booking-calendar",
-    //   icon: <Calendar />,
-    //   role: [ROLES.carer],
-    // },
-    // {
-    //   label: "Staff Allocation",
-    //   link: "/staff-allocation",
-    //   icon: <Allocation />,
-    //   role: [ROLES.coordinator],
-    // },
-
-    // {
-    //   label: "Training",
-    //   link: "/training",
-    //   icon: <TranieeInfo />,
-    //   role: [ROLES.coordinator],
-    // },
-    // {
-    //   label: "Settings",
-    //   link: "/settings",
-    //   icon: <Setting />,
-    //   role:"coordinator",
-    // },
-
-
-    // ####################### Care Coordinator Sidebar Items #######################
-    {
-      label: "Dashboard",
-      link: "/coordinator-dashboard",
-      icon: <Dashboard />,
-      role: [ROLES.coordinator],
-    },
-    {
-      label: "Profile",
-      link: "/care-coordinator-profile",
-      icon: <Profile />,
-      role: [ROLES.coordinator],
-    },
-    {
-      label: "Manage Shift",
-      link: "/shift-manager",
-      icon: <ShiftDetails />,
-      role: [ROLES.coordinator],
-    },
-    {
-      label: "Booking Calendar",
-      link: "/booking-calendar",
-      icon: <Calendar />,
-      role: [ROLES.coordinator],
-    },
-    {
-      label: "Staff Allocation",
-      link: "/staff-allocation",
-      icon: <Allocation />,
-      role: [ROLES.coordinator],
-    },
-    {
-      label: "Finance",
-      link: "/finance",
-      icon: <Finance />,
-      role: [ROLES.coordinator],
-      children: [
-        {
-          label: "Invoice",
-          link: "/finance/invoice",
-          icon: "",
-        },
-        {
-          label: "Staff Payment Details",
-          link: "/finance/staff-payment-details",
-          icon: "",
-        },
-        {
-          label: "Client Payment Details",
-          link: "/finance/client-payment-details",
-          icon: "",
-        },
-        {
-          label: "Reports",
-          link: "/finance/reports",
-          icon: "",
-        },
-      ],
-    },
-
-
-    // ####################### Client Sidebar Items #######################  
-    {
-      label: "Dashboard",
+      label: "HOME",
       link: "/client-dashboard",
       icon: <Dashboard />,
       role: [ROLES.client],
     },
     {
-      label: "Profile",
+      label: "OUR COLLECTION",
       link: "/client-profile",
       icon: <Profile />,
       role: [ROLES.client],
     },
     {
-      label: "Booking Calendar",
+      label: "CUSTOM ORDER",
       link: "/client-booking-calendar",
       icon: <Calendar />,
       role: [ROLES.client],
     },
+   
     {
-      label: "Manage Shift",
-      link: "/client-requested-shift",
-      icon: <Time />,
-      role: [ROLES.client],
-      children: [
-        {
-          label: "Requested Shifts",
-          link: "/client-requested-shift",
-          icon: "",
-        },
-        {
-          label: "Upcoming Shifts",
-          link: "/client-upcoming-shift",
-          icon: "",
-        },
-        {
-          label: "Completed Shifts",
-          link: "/client-completed-shift",
-          icon: "",
-        },
-        {
-          label: "Sign Off Shifts",
-          link: "/client-signoff-shift",
-          icon: "",
-        },
-      ],
-    },
-    {
-      label: "User Management",
+      label: "STYLE GUIDE",
       link: "/client-user-management",
       icon: <UserManagement />,
       role: [ROLES.client],
     },
     {
-      label: "Preferences",
+      label: "FITTING GUIDE",
       link: "/client-preferences",
       icon: <Preferences />,
       role: [ROLES.client],
     },
-
-
-    // ####################### Super Admin Sidebar Items #######################
     {
-      label: "Dashboard",
-      link: "/super-admin-dashboard",
-      icon: <Dashboard />,
-      role: [ROLES.superAdmin],
+      label: "MATERIAL GUIDE",
+      link: "/client-preferences",
+      icon: <Preferences />,
+      role: [ROLES.client],
     },
     {
-      label: "Manage User",
-      link: "/manage-user",
-      icon: <ManageUser />,
-      role: [ROLES.superAdmin],
+      label: "SOLE GUIDE",
+      link: "/client-preferences",
+      icon: <Preferences />,
+      role: [ROLES.client],
     },
     {
-      label: "Api Inventory",
-      link: "/api-inventory",
-      icon: <Inventory />,
-      role: [ROLES.superAdmin],
+      label: "SHOE CARE",
+      link: "/client-preferences",
+      icon: <Preferences />,
+      role: [ROLES.client],
     },
     {
-      label: "System Performance",
-      link: "/performance",
-      icon: <System />,
-      role: [ROLES.superAdmin],
+      label: "SHOPPING",
+      link: "/client-preferences",
+      icon: <Preferences />,
+      role: [ROLES.client],
     },
     {
-      label: "Back Up",
-      link: "/backup",
-      icon: <Backup />,
-      role: [ROLES.superAdmin],
+      label: "CONTACT",
+      link: "/client-preferences",
+      icon: <Preferences />,
+      role: [ROLES.client],
     },
-    {
-      label: "Manage Notification",
-      link: "/notifications",
-      icon: <Notifications />,
-      role: [ROLES.superAdmin],
-    },
-    {
-      label: "Audit Logs",
-      link: "/audit-logs",
-      icon: <Logs />,
-      role: [ROLES.superAdmin],
-    },
-
-
-    // ####################### Instructor Sidebar Items #######################
-    {
-      label: "Dashboard",
-      link: "/instructor-dashboard",
-      icon: <Dashboard />,
-      role: [ROLES.instructor],
-    },
-    {
-      label: "Profile",
-      link: "/instructor-profile",
-      icon: <Profile />,
-      role: [ROLES.instructor],
-    },
-    {
-      label: "Manage Courses",
-      link: "/manage-courses",
-      icon: <ManageCourse />,
-      role: [ROLES.instructor],
-    },
-    {
-      label: "Trainee Info",
-      link: "/trainee-info",
-      icon: <TranieeInfo />,
-      role: [ROLES.instructor],
-    },
-    {
-      label: "Webinar",
-      link: "/webinar/upcomming-webinar",
-      icon: <Webinar />,
-      role: [ROLES.instructor],
-      children: [
-        {
-          label: "Upcoming Webinar",
-          link: "/webinar/upcomming-webinar",
-          icon: "",
-        },
-        {
-          label: "Webinar History",
-          link: "/webinar/webinar-history",
-          icon: "",
-        },
-      ],
-    },
-
-
-    // ############################# Common Sidebar Items in Different Roles  #############################
-    {
-      label: "Training",
-      link: "/training",
-      icon: <TranieeInfo />,
-      role: [ROLES.carer, ROLES.coordinator],
-    },
-
-    {
-      label: "Reports",
-      link: "/reports",
-      icon: <Reports />,
-      role: [ROLES.carer, ROLES.coordinator, ROLES.instructor, ROLES.client, ROLES.admin, ROLES.superAdmin],
-    },
-
-    // to keep sidebar items in a order thats why kept here
-    {
-      label: "Ratings",
-      link: "/ratings/reviewed",
-      icon: <Ratings />,
-      role: [ROLES.carer, ROLES.coordinator, ROLES.client],
-      children: [
-        {
-          label: "Overall Ratings",
-          link: "/ratings/reviewed",
-          icon: "",
-        },
-        {
-          label: "Review Carers",
-          link: "/ratings/review-care-homes",
-          icon: "",
-        },
-      ],
-    },
-    {
-      label: "Rating & Feedback",
-      link: "/ratings",
-      icon: <Ratings />,
-      role: [ROLES.superAdmin],
-    },
-
-
-
-    {
-      label: "Settings",
-      link: "/settings",
-      icon: <Setting />,
-      role: [ROLES.admin, ROLES.coordinator, ROLES.client],
-    },
-
-    {
-      label: "Help",
-      link: "/help/FAQs",
-      icon: <Help />,
-      role: [ROLES.carer, ROLES.coordinator, ROLES.instructor, ROLES.client, ROLES.admin, ROLES.superAdmin],
-      children: [
-        {
-          label: "FAQs",
-          link: "/help/FAQs",
-          icon: "",
-        },
-        {
-          label: "IT Help Desk",
-          link: "/help/it-help-desk",
-          icon: "",
-        },
-      ],
-    },
+   
   ];
   const updateArray = SidebarMenues?.filter((ele) => ele?.role?.includes(role));
 
@@ -505,5 +109,5 @@ export const getSidebarMenues = (role: any, permissions: any) => {
     ?.filter((obj: any) => permissions?.some((ele: any) => formatString(ele?.name) === obj.label));
   const finalArray = role === 'system_admin' ? updateArray : permissionsValues
 
-  return finalArray
+  return SidebarMenues
 }
