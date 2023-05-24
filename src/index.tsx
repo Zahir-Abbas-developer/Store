2f77be0d4e0e50fb1b467c5908048654b2dc1881
zahir abbas <zahir.abbas@ceative.co.uk>
2023-05-24 14:07:56 +0500

    #fixed pagination and filters

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 const persistor = persistStore(store);
 
 root.render(
-  <React.StrictMode>
+  
     <BrowserRouter>
       <ConfigProvider theme={theme}>
       <Provider store={store}>
@@ -29,7 +29,7 @@ root.render(
   </Provider>,
       </ConfigProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
