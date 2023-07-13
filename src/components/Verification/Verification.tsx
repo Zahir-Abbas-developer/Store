22558fc9e90cddcd2d4cf342e328c29f14b220a5
zahir <zahir.abbas@ceative.co.uk>
2023-07-12 20:24:22 -0700

    add functionalities

--- src/components/Verification/Verification.tsx
+++ src/components/Verification/Verification.tsx
@@ -1,18 +1,30 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useVerifyUserMutation } from "../../store/Slices/Signin"
 import {  useEffect } from "react";
+import AppSnackbar from "../../utils/AppSnackbar";
 const Verification=()=>{
     const [verifyUser]=useVerifyUserMutation()
     const myParam = useLocation().search;
     const resetToken= new URLSearchParams(myParam).get("token");
     let navigate = useNavigate();
+    console.log(resetToken)
     const payload={
         token:resetToken
       }
+      const verifyUserDetails=async()=>{
+        try{
+       await verifyUser({payload}).unwrap()
+       AppSnackbar({ type: "success", messageHeading: "Congratulations!", message: "Your account is verified Successfully!" });
+       navigate("/login");
+        }
+        catch(error){
+            AppSnackbar({ type: "error", messageHeading: "Error!", message: "Something Wrong!" });
+        }
+      }
       useEffect(()=>{
         if(resetToken){
-        verifyUser({payload})
-        navigate("/login");
+           verifyUserDetails()
+    
         }
     },[])
     return(
